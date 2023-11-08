c3f9edb67c0efba7cf5d0cba6504f5bfa1dd3b6e
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-11-08 08:58:52 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'worker.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'webworker',
+};
