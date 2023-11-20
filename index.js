f8b70b6cc883e9c4a7ed2bae3bb1853400bc353d
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-11-20 15:12:00 +0800

    Update index.js
    dfff

--- index.js
+++ index.js
@@ -1,61 +1,60 @@
 const dnsPacket = require('dns-packet')
 const Buffer = require('buffer').Buffer
 
-const DOH_ADDRESS = "cloudflare-dns.com/dns-query"
+const DOH_ADDRESS = "dns.google/dns-query"
 
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+const r404 = new Response(null, {status: 404});
+
+// developers.cloudflare.com/workers/runtime-apis/fetch-event/#syntax-module-worker
+export default {
+    async fetch(r, env, ctx) {
+        return handleRequest(r);
+    },
+};
 
 async function handleRequest(request) {
-  const url = new URL(request.url)
-  const { pathname, search } = url
+    let res = r404;
+    const { method, headers, url } = request
+    let clientIp =headers.get('CF-Connecting-IP');
+    const sourcePrefixLength = clientIp.includes(':') ? 48 : 24;
+    //const ecs = '&edns_client_subnet=221.179.3.0/24&ecs=221.179.3.0/24';//clientIp?`&edns_client_subnet=${clientIp}/${sourcePrefixLength}`:'';
+    const searchParams = new URL(url).searchParams
 
-  if (pathname == "/") {
-    return new Response(`200 OK`, { status: 200 })
-  }
-
-  if (request.method !== "GET" && request.method !== "POST") {
-    return new Response(`Method ${request.method} not allowed.`, { status: 405 })
-  }
-
-  const clientIp = request.headers.get('CF-Connecting-IP')
-
-  if (!clientIp) {
-    throw new Error('Client IP not found in request headers')
-  }
-
-  const sourcePrefixLength = clientIp.includes(':') ? 48 : 24
-
-  const body = await request.clone().arrayBuffer()
-  const dnsMsg = dnsPacket.decode(Buffer.from(body))
-
-  const ecsOption = {
-    code: 'CLIENT_SUBNET',
-    ip: clientIp,
-    sourcePrefixLength: sourcePrefixLength,
-    scopePrefixLength: 0
-  }
-
-  dnsMsg.additionals.push({
-    type: 'OPT',
-    name: '.',
-    udpPayloadSize: 4096,
-    options: [ecsOption]
-  })
-
-  dnsMsg.flags |= (1 << 15)
-
-  const modifiedBody = dnsPacket.encode(dnsMsg)
-
-  const newURL = `https://${DOH_ADDRESS}`
-  const newRequest = new Request(newURL, {
-    body: modifiedBody,
-    headers: request.headers,
-    method: request.method,
-    redirect: request.redirect
-  })
-  console.log(newRequest.headers)
-
-  return await fetch(newRequest)
+    if (method == 'GET' && searchParams.has('dns')) {
+        const query_string = searchParams.get('dns')  
+        if (query_string == "REDACTED"){
+            return new Response('', {status: 200})
+        }
+        //dns packet
+        //const body = await request.clone().arrayBuffer()
+        const dnsMsg = dnsPacket.decode(Buffer.atob(query_string))
+      
+        const ecsOption = {
+          code: 'CLIENT_SUBNET',
+          ip: clientIp,
+          sourcePrefixLength: sourcePrefixLength,
+          scopePrefixLength: 0
+        }
+      
+        dnsMsg.additionals.push({
+          type: 'OPT',
+          name: '.',
+          udpPayloadSize: 4096,
+          options: [ecsOption]
+        })
+      
+        dnsMsg.flags |= (1 << 15)
+      
+        const modifiedBody = dnsPacket.encode(dnsMsg)
+      
+        const newURL = `https://${DOH_ADDRESS}`
+        const newRequest = new Request(newURL, {
+          body: modifiedBody,
+          headers: headers,
+          method: "POST",
+        })
+        //
+        res = await fetch(newRequest)
+    } 
+    return res;
 }
