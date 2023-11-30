869cfe2f5f1249ee9713be209409840dd041510e
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-11-30 23:57:11 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import * as dnsPacket from 'dns-packet'
-import { Buffer } from 'buffer'
+import * as dnsPacket from 'node:dns-packet'
+import { Buffer } from 'node:buffer'
 
 const DOH_ADDRESS = "cloudflare-dns.com/dns-query"
 
