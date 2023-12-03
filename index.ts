36984de7e734b214c3ec92cfd3df8ccf32c2c73a
wingeva1986 <35324882+wingeva1986@users.noreply.github.com>
2023-12-03 21:36:42 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -83,7 +83,7 @@ export default {
 	//ctx.waitUntil(socket.close());
        
        const startTime = performance.now();
-       const modifiedBody = dnsPacket.encode(dnsMsg).toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
+       const modifiedBody = dnsPacket.encode(dnsMsg).toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '%3D');
        console.log(modifiedBody)
        const newRequest = new Request(DOH_ADDRESS+ modifiedBody.substring(5), {
           //body: modifiedBody,
